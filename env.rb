17ecfe57a643297d8a096ed669a926597d60f548
delasthanos <delasthanosdev@gmail.com>
2024-08-27 18:06:15 +0300

    Github workflow
    Trigger workflow
    
    Headless webdriver option

--- env.rb
+++ env.rb
@@ -10,6 +10,7 @@ STAFF_URL = "#{BASE_URL}/staff".freeze
 
 Capybara.register_driver :firefox do |app|
   options = Selenium::WebDriver::Firefox::Options.new
+  options.add_argument("--headless")
 
   Capybara::Selenium::Driver.new(app, browser: :firefox, options:)
 end
