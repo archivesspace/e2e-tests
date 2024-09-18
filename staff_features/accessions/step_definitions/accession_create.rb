6502dd60b90f2284b7625385c05c2a77de906f8d
blacksmith-welder <delasthanosdev@gmail.com>
2024-09-18 13:40:54 +0300

    Ensure accession is created

--- staff_features/accessions/step_definitions/accession_create.rb
+++ staff_features/accessions/step_definitions/accession_create.rb
@@ -4,3 +4,14 @@ Given('the user is on the New Accession page') do
   click_on 'Create'
   click_on 'Accession'
 end
+
+Then('the Accession is created') do
+  uri = find('#accession_form')[:'data-update-monitor-record-uri']
+
+  @accession_id = uri.split('/').pop
+
+  visit "#{STAFF_URL}/accessions/#{@accession_id}/edit"
+
+  expect(find('h2').text).to eq "#{@uuid} Accession"
+  expect(find('#accession_id_0_').value).to eq @uuid
+end
