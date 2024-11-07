bddf0cee43b7738ade2b9fc27c10dd82c268ef1e
Lora Woodford <lorawoodford@gmail.com>
2024-11-07 12:53:07 -0500

    Add jobs

---
+++ staff_features/jobs/step_definitions/jobs.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+Given('the user is on the Import Job page') do
+  click_on 'Create'
+  click_on 'Background Job'
+  click_on 'Import Data'
+end
+
+When 'the user adds {string} as a file' do |file_path|
+  attach_file('files[]', file_path, make_visible: true)
+end
+
+Then 'the Import Job page is displayed' do
+  expect(find('h2').text).to start_with 'Import Job'
+end
+
+Then 'the job completes' do
+  expect(page).not_to have_text 'This job is next in the queue.'
+  expect(page).to have_text 'The job has completed.'
+end
+
+Then 'the New & Modified Records section contains {string} links' do |number|
+  links = all('#jobRecordsSpool > div.subrecord-form-fields > div > a')
+  expect(links.count).to eq(number.to_i)
+end
+
+Then 'the record links do not display {string}' do |string|
+  expect(all('span.badge.badge-warning').map(&:text)).not_to include(string)
+end
