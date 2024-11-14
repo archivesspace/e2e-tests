23c17e6204a9d2beca0fb9a804e94bb303d3ef26
Konstantina Dimopoulou <13031341+kdimopulu@users.noreply.github.com>
2024-11-14 09:01:55 +0200

    Accession Event create (#16)
    Accession Event create #16

--- staff_features/accessions/step_definitions/accession_edit.rb
+++ staff_features/accessions/step_definitions/accession_edit.rb
@@ -40,27 +40,6 @@ Given 'the Accession is opened in the view mode' do
   click_on 'View'
 end
 
-Given 'the Accession is opened in edit mode' do
-  visit "#{STAFF_URL}/accessions"
-
-  fill_in 'filter-text', with: @uuid
-
-  within '.search-filter' do
-    find('button').click
-  end
-
-  search_result_rows = all('#tabledSearchResults tbody tr')
-  expect(search_result_rows.length).to eq 1
-
-  within search_result_rows[0] do
-    element = find('a', text: 'Edit')
-
-    @accession_id = URI.decode_www_form_component(element[:href]).split('/').pop
-  end
-
-  click_on 'Edit'
-end
-
 Then 'the Accession is updated with the new {string} as {string}' do |field, value|
   fill_in field, with: value
 end
--- staff_features/shared/step_definitions.rb
+++ staff_features/shared/step_definitions.rb
@@ -9,6 +9,7 @@ Given 'an administrator user is logged in' do
 
   ensure_test_repository_exists
   ensure_test_user_exists
+  ensure_test_agent_exists
 end
 
 Given 'an archivist user is logged in' do
@@ -17,6 +18,13 @@ end
 
 When 'the user clicks on {string}' do |string|
   click_on_string string
+end
+
+When 'the user clicks on {string} in the dropdown menu' do |string|
+  within '.dropdown-menu' do |dropdown_menu|
+    elements = dropdown_menu.all(:xpath, "//*[contains(text(), '#{string}')]")
+    elements[1].click
+  end
 end
 
 When 'the user clicks on {string} in the confirm popup' do |string|
@@ -39,8 +47,28 @@ When 'the user fills in {string} with {string}' do |label, value|
   fill_in label, with: value
 end
 
+When 'the user fills in {string} with {string} in the {string} form' do |label, value, form_title|
+  section_title = find('h3', text: form_title)
+  section = section_title.ancestor('section')
+  expect(section[:id]).to_not eq nil
+
+  within section do
+    fill_in label, with: value
+  end
+end
+
 When 'the user selects {string} from {string}' do |option, label|
   select option, from: label
+end
+
+When 'the user selects {string} from {string} in the {string} form' do |option, label, form_title|
+  section_title = find('h3', text: form_title)
+  section = section_title.ancestor('section')
+  expect(section[:id]).to_not eq nil
+
+  within section do
+    select option, from: label
+  end
 end
 
 When 'the user checks {string}' do |label|
@@ -91,6 +119,14 @@ end
 
 Then 'the {string} is checked' do |label|
   expect(page).to have_field(label, checked: true)
+end
+
+Then 'only the following error message is displayed' do |messages|
+  expect(messages.raw.length).to eq 1
+
+  messages.raw.each do |message|
+    expect(page).to have_text message[0]
+  end
 end
 
 Then 'the {string} section is displayed' do |section_heading|
