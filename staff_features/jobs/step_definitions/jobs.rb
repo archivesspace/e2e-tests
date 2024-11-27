7fa65766e90d7d40a31e085e0cc21de94fb88758
blacksmith-welder <delasthanosdev@gmail.com>
2024-11-27 11:51:16 +0200

    Fix import data failing test

--- staff_features/jobs/step_definitions/jobs.rb
+++ staff_features/jobs/step_definitions/jobs.rb
@@ -15,8 +15,17 @@ Then 'the Import Job page is displayed' do
 end
 
 Then 'the job completes' do
-  expect(page).not_to have_text 'This job is next in the queue.'
-  expect(page).to have_text 'The job has completed.'
+  tries = 0
+
+  begin
+    expect(page).to_not have_text 'This job is next in the queue.'
+    expect(page).to have_text 'The job has completed.'
+  rescue RSpec::Expectations::ExpectationNotMetError => e
+    tries += 1
+    sleep 3
+
+    raise e if tries == 5
+  end
 end
 
 Then 'the New & Modified Records section contains {int} links' do |number|
