2959045257a2e34006893402c56cdebe28cfc6cc
blacksmith-welder <delasthanosdev@gmail.com>
2024-12-02 17:42:02 +0200

    Resource publish

--- staff_features/resources/step_definitions/resource_edit.rb
+++ staff_features/resources/step_definitions/resource_edit.rb
@@ -23,11 +23,6 @@ Given 'the Resource is opened in the view mode' do
   visit "#{STAFF_URL}/resources/#{@resource_id}"
 end
 
-Given 'the Resource is opened in edit mode' do
-  visit "#{STAFF_URL}/resources/#{@resource_id}/edit"
-  wait_for_ajax
-end
-
 Given 'the Resource has one Language' do
   languages = all('#resource_lang_materials_ .subrecord-form-list .subrecord-form-wrapper')
 
