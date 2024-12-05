1bd57ab432de98aadbc39d97078f96f46aeefdf5
blacksmith-welder <delasthanosdev@gmail.com>
2024-12-05 13:10:21 +0200

    Use wait time for completed job

--- staff_features/jobs/step_definitions/jobs.rb
+++ staff_features/jobs/step_definitions/jobs.rb
@@ -15,16 +15,9 @@ Then 'the Import Job page is displayed' do
 end
 
 Then 'the job completes' do
-  tries = 0
-
-  begin
+  using_wait_time(15) do
     expect(page).to_not have_text 'This job is next in the queue.'
     expect(page).to have_text 'The job has completed.'
-  rescue RSpec::Expectations::ExpectationNotMetError => e
-    tries += 1
-    sleep 3
-
-    raise e if tries == 5
   end
 end
 
