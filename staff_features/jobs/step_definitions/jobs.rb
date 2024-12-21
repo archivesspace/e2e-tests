4d4ec2eef19a318722198e4ab6a4d8fc0c3930ad
blacksmith-welder <delasthanosdev@gmail.com>
2024-12-21 09:36:33 +0200

    Fix import data failing test

--- staff_features/jobs/step_definitions/jobs.rb
+++ staff_features/jobs/step_definitions/jobs.rb
@@ -15,9 +15,15 @@ Then 'the Import Job page is displayed' do
 end
 
 Then 'the job completes' do
-  using_wait_time(15) do
+  tries = 0
+  begin
     expect(page).to_not have_text 'This job is next in the queue.'
     expect(page).to have_text 'The job has completed.'
+  rescue RSpec::Expectations::ExpectationNotMetError => e
+    tries += 1
+    sleep 3
+
+    raise e if tries == 5
   end
 end
 
