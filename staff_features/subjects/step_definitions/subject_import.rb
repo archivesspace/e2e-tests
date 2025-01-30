f61b16a3ecac106aa6599533a1aed61b8c309076
blacksmith-welder <delasthanosdev@gmail.com>
2025-01-30 20:39:59 +0200

    Subject import

---
+++ staff_features/subjects/step_definitions/subject_import.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+When 'the user clicks on the gear icon' do
+  within '.repo-container' do
+    find('.btn.btn-default.navbar-btn.dropdown-toggle').click
+  end
+end
+
+When 'the user checks {string} in the LCNAF Import form' do |string|
+  elements = all(:css, "div[class*='radio']")
+
+  elements.each do |element|
+    element.find('input').click if element.text == string
+  end
+end
+
+When 'the user selects the first Subject from the search results' do
+  elements = all('#results div.lcnaf-result')
+  elements[0].find('button').click
+end
