207ae2148668f6f70e26f5979e363463e09fc7d5
Konstantina Dimopoulou <13031341+kdimopulu@users.noreply.github.com>
2025-02-06 06:24:39 +0200

    Subject edit default values (#79)

--- staff_features/digital_objects/step_definitions/digital_object_shared.rb
+++ staff_features/digital_objects/step_definitions/digital_object_shared.rb
@@ -204,3 +204,16 @@ end
 And 'the user selects the Digital Object Component' do
   click_on "Digital Object Component Label #{@uuid}"
 end
+
+Then 'the Assessment is linked to the Digital Object in the {string} form' do |form_title|
+  section_title = find('h3', text: form_title)
+  section = section_title.ancestor('section')
+  expect(section[:id]).to_not eq nil
+
+  related_accessions_elements = section.all('li.token-input-token')
+
+  expect(related_accessions_elements.length).to eq 1
+  related_accession = related_accessions_elements[0].find('.digital_object')
+
+  expect(related_accession[:'data-content']).to include "digital_objects/#{@digital_object_id}"
+end
