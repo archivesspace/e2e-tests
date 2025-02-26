cbebeeb6aea1ca8757e087589a1558d7f3fdfc3c
blacksmith-welder <delasthanosdev@gmail.com>
2025-02-26 11:52:54 +0200

    Remove duplicate steps

--- staff_features/agents/step_definitions/agent_view.rb
+++ staff_features/agents/step_definitions/agent_view.rb
@@ -142,5 +142,5 @@ Then 'the Agent is in the search results' do
 end
 
 Then 'the Agent view page is displayed' do
-  expect(find('h2').text).to eq "#{@uuid} Agent"
+  expect(find('h2').text).to eq "Agent #{@uuid} Agent"
 end
