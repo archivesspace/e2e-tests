aac05635f6bddaf3d2f089e61fe6d260865bee72
Konstantina Dimopoulou <13031341+kdimopulu@users.noreply.github.com>
2025-03-19 17:57:04 +0200

    Digital Object Rapid Data Entry (#73)

--- staff_features/digital_objects/step_definitions/digital_object_rapid_data_entry.rb
+++ staff_features/digital_objects/step_definitions/digital_object_rapid_data_entry.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
-Then 'a new template with name {string} with the following data is added to the Resource Rapid Data Entry templates' do |template, form_values_table|
-  visit "#{STAFF_URL}/resources/#{@resource_id}/edit"
+Then 'a new template with name {string} with the following data is added to the Digital Object Rapid Data Entry templates' do |template, form_values_table|
+  visit "#{STAFF_URL}/digital_objects/#{@digital_object_id}/edit"
   click_on 'Rapid Data Entry'
   wait_for_ajax
 
@@ -29,8 +29,8 @@ Then 'a new template with name {string} with the following data is added to the 
   end
 end
 
-Given 'a Resource Rapid Data Entry template has been created' do
-  visit "#{STAFF_URL}/resources/#{@resource_id}/edit"
+Given 'a Digital Object Rapid Data Entry template has been created' do
+  visit "#{STAFF_URL}/digital_objects/#{@digital_object_id}/edit"
   click_on 'Rapid Data Entry'
   wait_for_ajax
 
@@ -38,15 +38,36 @@ Given 'a Resource Rapid Data Entry template has been created' do
   fill_in 'templateName', with: "RDE Template #{@uuid}"
   click_on 'Save Template'
 
-  visit "#{STAFF_URL}/resources/#{@resource_id}/edit"
+  visit "#{STAFF_URL}/digital_objects/#{@digital_object_id}/edit"
   wait_for_ajax
 end
 
-Then 'the template is removed from the Resource Rapid Data Entry templates' do
-  visit "#{STAFF_URL}/resources/#{@resource_id}/edit"
+Then 'the template is removed from the Digital Object Rapid Data Entry templates' do
+  visit "#{STAFF_URL}/digital_objects/#{@digital_object_id}/edit"
   click_on 'Rapid Data Entry'
   wait_for_ajax
   click_on 'Apply an RDE Template'
 
   expect(find('.dropdown-menu')).to_not have_text "RDE Template #{@uuid}"
+end
+
+Then 'the {string} has value {string} in all rows' do |label, value|
+  table_header_cells = all('.fieldset-labels .kiketable-th-text')
+
+  label_position = 0
+  table_header_cells.each_with_index do |header, index|
+    next if header.text != label
+
+    label_position = index
+    break
+  end
+
+  label_position += 1
+  expect(label_position).to_not eq 0
+
+  table_rows = all('#rdeTable tbody tr')
+  table_rows.each do |row|
+    cell = row.all('td')
+    expect(cell[label_position].find('input, select').value.downcase).to eq value.downcase
+  end
 end
--- staff_features/shared/step_definitions.rb
+++ staff_features/shared/step_definitions.rb
@@ -427,3 +427,78 @@ When 'the user clicks on the gear icon' do
     find('.btn.btn-default.navbar-btn.dropdown-toggle').click
   end
 end
+
+When 'the user selects {string} from {string} in the first row of the Rapid Data Entry table' do |option, label|
+  table_header_cells = all('.fieldset-labels .kiketable-th-text')
+
+  label_position = 0
+  table_header_cells.each_with_index do |header, index|
+    label_position = index if header.text == label
+  end
+
+  label_position += 1
+  expect(label_position).to_not eq 0
+
+  table_field_cells = all('#rdeTable tbody td')
+  field_cell = table_field_cells[label_position]
+  field_cell_select = field_cell.find('select')
+  field_cell_select.select option
+end
+
+When 'the user fills in {string} with {string} in the first row of the Rapid Data Entry table' do |label, value|
+  table_header_cells = all('.fieldset-labels .kiketable-th-text')
+
+  label_position = 0
+  table_header_cells.each_with_index do |header, index|
+    label_position = index if header.text == label
+  end
+
+  label_position += 1
+  expect(label_position).to_not eq 0
+
+  table_field_cells = all('#rdeTable tbody td')
+  field_cell = table_field_cells[label_position]
+  field_cell_input = field_cell.find('input')
+  field_cell_input.fill_in with: value
+end
+
+When 'the user unchecks the {string} checkbox in the dropdown menu' do |label|
+  uncheck label
+end
+
+Then 'the {string} column is no longer visible in the Rapid Data Entry table' do |string|
+  expect(page).to_not have_css('.fieldset-label.sticky.kiketable-th', text: string)
+end
+
+Then 'a new row with the following data is added to the Rapid Data Entry table' do |form_values_table|
+  table_header_cells = all('.fieldset-labels .kiketable-th-text')
+  table_field_rows = all('#rdeTable tbody tr')
+
+  expect(table_field_rows.length).to eq 2
+
+  form_values_hash = form_values_table.rows_hash
+  form_values_hash.each do |field, value|
+    field_position = 0
+    table_header_cells.each_with_index do |header, index|
+      field_position = index if header.text == field
+    end
+    field_position += 1
+    expect(field_position).to_not eq 0
+
+    table_field_cells = table_field_rows[1].all('td')
+    field_cell = table_field_cells[field_position]
+
+    expect(field_cell.find('input, select, textarea').value.downcase.gsub(' ', '_')).to eq value.downcase.gsub(' ', '_')
+  end
+end
+
+Given 'a Rapid Data Entry template has been created' do
+  click_on 'Rapid Data Entry'
+  click_on 'Save as Template'
+  fill_in 'Template name', with: "RDE Template  #{@uuid}"
+  click_on 'Save Template'
+end
+
+When 'the user checks the created Rapid Data Entry template' do
+  find('label', text: "RDE Template #{@uuid}").click
+end
